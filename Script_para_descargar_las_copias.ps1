4a576f0c0ad9a34603aaf6fbb7e7a0023d3d2ead
TFMResiliencia <140199212+TFMResiliencia@users.noreply.github.com>
2023-07-21 20:31:59 +0200

    Update Script_para_descargar_las_copias.ps1

--- Script_para_descargar_las_copias.ps1
+++ Script_para_descargar_las_copias.ps1
@@ -2,5 +2,5 @@
 $file = Get-GcsObject -Bucket "tfm_resiliencia_copias_de_seguridad"  | Select Name, TimeCreated| Sort-Object TimeCreated -Descending| Select-Object -First 1 |Select -ExpandProperty Name 
 
 #Descarga el archivo mas reciente
-Read-GcsObject  -Bucket "tfm_resiliencia_copias_de_seguridad" -ObjectName $file  -OutFile "C:\Users\tfm\Desktop\Copia_de_seguridad\$file"
+Read-GcsObject  -Bucket "tfm_resiliencia_copias_de_seguridad" -ObjectName $file  -OutFile "C:\Users\tfm\Desktop\Copia_de_seguridad\$file" -Force
 
