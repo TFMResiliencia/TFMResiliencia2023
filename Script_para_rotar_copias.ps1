d173cd0d49bbbddd6d0db10b3e21cef3c3a95fcd
TFMResiliencia <140199212+TFMResiliencia@users.noreply.github.com>
2023-07-27 01:40:07 +0200

    Update Script_para_rotar_copias.ps1
    Corregido error derivado de la subida del archivo desactualizado

--- Script_para_rotar_copias.ps1
+++ Script_para_rotar_copias.ps1
@@ -3,7 +3,7 @@ $contadorArchivos = (Get-GcsObject -Bucket "tfm_resiliencia_copias_de_seguridad"
 
 #Si existen 3 o más copias se borra el más antiguo
 if ($contadorArchivos -ge 3) {
-	$file = Get-GcsObject -Bucket "tfm_resiliencia_copias_de_seguridad"  | Select Name, TimeCreated| Sort-Object TimeCreated -Descending| Select-Object -First 1 |Select -ExpandProperty Name 
+	$file = Get-GcsObject -Bucket "tfm_resiliencia_copias_de_seguridad"  | Select Name, TimeCreated| Sort-Object TimeCreated | Select-Object -First 1 |Select -ExpandProperty Name 
 	Remove-GcsObject -Bucket "tfm_resiliencia_copias_de_seguridad" -ObjectName $file
 	Write-Host "Borrada la copia más antigua"
 }
